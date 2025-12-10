e86beed54118a2e1539cb4bc92a441481d19be12
santiago gutierrez <santiagogutierrezhernandez1012@gmail.com>
2025-12-09 22:43:35 -0300

    Refactor PhysicsBody class for clarity and maintainability

--- Nez.Portable/ECS/Components/Physics/PhysicsBody.cs
+++ Nez.Portable/ECS/Components/Physics/PhysicsBody.cs
@@ -3,15 +3,19 @@ using Microsoft.Xna.Framework;
 namespace Nez
 {
     /// <summary>
-    /// Simple physics body that applies its velocity each frame using an IMover on the same Entity.
+    /// Simple physics body that applies its velocity each frame using an IMover on the same Entity. Its update order is set to int.MaxValue
+    /// on the constructor so it runs after most other logic.
     /// </summary>
     public class PhysicsBody : Component, IUpdatable
     {
-        [Inspectable]
-        public Vector2 Velocity;
+        [Inspectable] public Vector2 Velocity;
         public bool MoveOnUpdate = true;
 
         IMover _mover;
+
+        public PhysicsBody(){
+            UpdateOrder = int.MaxValue;
+        }
 
         public override void OnAddedToEntity()
         {
@@ -24,8 +28,10 @@ namespace Nez
             if (!MoveOnUpdate || _mover == null)
                 return;
 
-            var motion = Velocity * Time.DeltaTime;
+            var motion = CalculateMotion();
             _mover.Move(motion);
         }
+
+        public virtual Vector2 CalculateMotion() => Velocity * Time.DeltaTime;
     }
 }
